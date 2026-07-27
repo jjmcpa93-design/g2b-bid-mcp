c74f32347396ed30a2724d40b9dc46406b223a8c
jjmcpa93-design <jjmcpa93@gmail.com>
2026-07-27 15:12:22 +0900

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,5 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ko">
-      <body>{children}</body>
-    </html>
-  );
-}
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
